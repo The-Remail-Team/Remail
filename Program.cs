b9a1b8470c3bff093fdf25e6365ea49fba9214e5
Busaruba2011 <owensteele2011@gmail.com>
2020-12-11 17:37:09 +0000

    Add code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RemailVersionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are using Remail 1.0.0");
+            Console.WriteLine("Go to busaruba2011.github.io/Remail for more info");
+            Console.WriteLine("Github: @Busaruba2011");
+
+        }
+    }
+}
