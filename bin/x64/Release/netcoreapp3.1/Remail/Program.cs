5131b7472b6cac72fef80f24a175583012727ad4
Busaruba2011 <65495333+Busaruba2011@users.noreply.github.com>
2020-12-13 11:14:19 -0800

    Update Program.cs

---
+++ bin/x64/Release/netcoreapp3.1/Remail/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RemailVersionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are using Remail 1.0.0");
+            Console.WriteLine("Go to the-remail-team.github.io/Remail for more info");
+            Console.WriteLine("Github: @The-Remail-Team");
+            
+
+        }
+    }
+}
