d2c6e7424cc61785b5dca5ad747f99a02892966b
Busaruba2011 <65495333+Busaruba2011@users.noreply.github.com>
2020-12-20 06:48:32 -0800

    Add files via upload

---
+++ BatchVersion/RemailVersionCheck.bat
@@ -0,0 +1,4 @@
+@echo
+echo You are using Remail 1.0.0 with version check.
+echo Please see the website for more info: the-remail-team.github.io 
+echo See the GitHub page for more info: bit.ly/The-Remail-Team-GitHub
